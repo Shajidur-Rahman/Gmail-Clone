d2ef2bcb4f9a64bcc428e7e2aedb5b07ac36e62c
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-03-21 21:51:37 +0600

    recyclerView added

--- app/src/main/java/com/shajidurrahman/gmailclone/ui/home/HomeFragment.java
+++ app/src/main/java/com/shajidurrahman/gmailclone/ui/home/HomeFragment.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.shajidurrahman.gmailclone.CustomAdapter;
+import com.shajidurrahman.gmailclone.R;
 import com.shajidurrahman.gmailclone.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    String[] arr = {"sdfl", "sdfl", "sdfl", "sdfl", "sdfl", "sdfl", "sdfl"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +31,9 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        CustomAdapter cs = new CustomAdapter(arr);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerView.setAdapter(cs);
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
